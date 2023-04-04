b962b0c610fb8773259afadd9125202c9a07ac75
startlinkpython <startlink.dev.python@gmail.com>
2023-04-04 06:46:06 +0000

    04042023

--- hola.py
+++ hola.py
@@ -1,1 +1,10 @@
 print("Hola, este es mi primer programa en Python, con cambios nuevos...")
+
+nombre = input ("Introduzca su nombre : ")
+edad = int(input ("Introduzca su edad : "))
+
+n = 2021 - edad     # Esto es un comentario para explicar el como se calcula el año de nacimiento
+
+print (nombre , "naciste en el año : ", n)
+
+
